7dd734abfd8af0d9483f8a77fa372824ab366f4b
felixoder <debayanghosh408@gmail.com>
2025-01-18 21:51:45 +0530

    deployment bug

--- src/app/(routes)/@modal/(.)posts/[id]/page.tsx
+++ src/app/(routes)/@modal/(.)posts/[id]/page.tsx
@@ -3,7 +3,7 @@ import ModalPostContent from "@/components/ModalPostContent";
 import Preloader from "@/components/Preloader";
 import {Suspense} from "react";
 
-export default async function PostInModal({params}:{params: {id: string}}) {
+export default async function PostInModal({params}:any) {
   return (
     <Modal>
       <Suspense fallback={<Preloader />}>
