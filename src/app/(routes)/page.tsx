b612c251a397b0b96e300cabb79ef18079178a77
felixoder <debayanghosh408@gmail.com>
2025-01-19 01:32:21 +0530

    redirections

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,10 +1,18 @@
 import {auth, signIn} from "@/auth";
 import Preloader from "@/components/Preloader";
 import UserHome from "@/components/UserHome";
+import { prisma } from "@/db";
+import { redirect } from "next/navigation";
 import {Suspense} from "react";
 
 export default async function Home() {
   const session = await auth();
+  const profile = await prisma.profile.findFirst({
+    where: {email: session?.user?.email as string}
+  })
+  if(!profile){
+    return redirect('/settings')
+  }
   return (
     <div className="">
       {session && (
