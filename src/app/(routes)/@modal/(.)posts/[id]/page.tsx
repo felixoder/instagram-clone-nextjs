e24e9d3656dbb53da1dd49209314033af2167868
felixoder <debayanghosh408@gmail.com>
2025-01-18 23:15:26 +0530

    @modal problem-1

--- src/app/(routes)/@modal/(.)posts/[id]/page.tsx
+++ src/app/(routes)/@modal/(.)posts/[id]/page.tsx
@@ -1,16 +1,25 @@
+import { getSinglePostdata } from "@/components/actions";
 import Modal from "@/components/Modal";
-import ModalPostContent from "@/components/ModalPostContent";
 import Preloader from "@/components/Preloader";
+import SinglePostContent from "@/components/SinglePostContent";
 import { Suspense } from "react";
 
 // Async function to await params
 export default async function PostInModal({ params }: { params: { id: string } }) {
-  const { id } = params; // Await params to get dynamic route parameters
+    const { id } = params; // Await params to get dynamic route parameters
+    const {post, authorProfile, comments, commentsAuthors, myLike, myBookmark} = await getSinglePostdata(id);
 
   return (
     <Modal>
       <Suspense fallback={<Preloader />}>
-        <ModalPostContent postId={id} />
+         <SinglePostContent
+                post={post}
+                authorProfile={authorProfile}
+                comments={comments}
+                commentsAuthors={commentsAuthors}
+                myLike={myLike}
+               myBookmark={myBookmark}
+        />
       </Suspense>
     </Modal>
   );
