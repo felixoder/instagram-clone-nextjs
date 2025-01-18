8b86c3bd4bc90687f65ffdbb03041846c3edb8e1
felixoder <debayanghosh408@gmail.com>
2025-01-19 01:30:10 +0530

    redirections

--- src/app/(routes)/profile/page.tsx
+++ src/app/(routes)/profile/page.tsx
@@ -1,13 +1,14 @@
 import {auth} from "@/auth";
 import ProfilePageContent from "@/components/ProfilePageContent";
 import {prisma} from "@/db";
+import {redirect} from "next/navigation";
 
 export default async function ProfilePage() {
   const session = await auth();
   const profile = await prisma.profile
     .findFirst({where:{email:session?.user?.email as string}});
   if (!profile) {
-    return 'not logged in'
+    return redirect('/settings');
   }
   return (
     <ProfilePageContent
