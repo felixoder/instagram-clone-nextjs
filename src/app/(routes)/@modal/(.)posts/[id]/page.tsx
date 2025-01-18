81a96081df23e7bf2c8e67d369627689d4874d53
felixoder <debayanghosh408@gmail.com>
2025-01-18 22:07:19 +0530

    deployment bug-prefetch

--- src/app/(routes)/@modal/(.)posts/[id]/page.tsx
+++ src/app/(routes)/@modal/(.)posts/[id]/page.tsx
@@ -1,9 +1,13 @@
 import Modal from "@/components/Modal";
 import ModalPostContent from "@/components/ModalPostContent";
 import Preloader from "@/components/Preloader";
-import {Suspense} from "react";
+import { Suspense } from "react";
 
-export default async function PostInModal({params:{id}}:{params:{id:string}}) {
+// Adjust the type of `params` to match the expected Promise structure
+export default async function PostInModal({ params }: { params: Promise<{ id: string }> }) {
+  const resolvedParams = await params; // Resolve the Promise
+  const { id } = resolvedParams;
+
   return (
     <Modal>
       <Suspense fallback={<Preloader />}>
@@ -12,3 +16,4 @@ export default async function PostInModal({params:{id}}:{params:{id:string}}) {
     </Modal>
   );
 }
+
